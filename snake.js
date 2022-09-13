f09458203a40d738400a271cfab7655e082b70f2
Omarolanda <94382866+Omarolanda@users.noreply.github.com>
2022-09-13 13:21:33 +0200

    function update

--- snake.js
+++ snake.js
@@ -6,3 +6,9 @@ context.fillRect(0, 0 ,400, 400);
 
 context.fillStyle = "white";
 context.fillRect(200, 200 ,20, 20);
+
+function update(){
+  console.log("update");
+}
+
+setInterval(update , 1000);
