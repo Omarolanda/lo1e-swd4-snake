67fafeb4177b922097e3a3c9c8886324846df70a
Omarolanda <94382866+Omarolanda@users.noreply.github.com>
2022-09-13 13:14:11 +0200

    kleur veranderd

--- snake.js
+++ snake.js
@@ -1,8 +1,8 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 
-context.fillStyle = "lightblue";
+context.fillStyle = "black";
 context.fillRect(0, 0 ,400, 400);
 
-context.fillStyle = "aqua";
-context.fillRect(200, 200 ,40, 40);
+context.fillStyle = "white";
+context.fillRect(200, 200 ,20, 20);
