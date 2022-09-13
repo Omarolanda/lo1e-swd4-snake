d3cbf16356d20292383493808cdf5b224f6d1034
Omarolanda <94382866+Omarolanda@users.noreply.github.com>
2022-09-13 14:02:13 +0200

    functions maken

--- snake.js
+++ snake.js
@@ -1,14 +1,39 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 
-context.fillStyle = "black";
-context.fillRect(0, 0 ,400, 400);
+let snakeX = 200;
+let snakeY = 200;
+let direction = "up";
 
-context.fillStyle = "white";
-context.fillRect(200, 200 ,20, 20);
-
-function update(){
-  console.log("update");
+function drawBackground() {
+  context.fillStyle = "black";
+  context.fillRect(0, 0, 400, 400);
 }
 
-setInterval(update , 1000);
+function drawSnake() {
+  context.fillStyle = "white";
+  context.fillRect(snakeX, snakeY, 20, 20);
+}
+
+function update() {
+  if (direction == "right") {
+    snakeX += 20;
+  } else if (direction == "left") {
+    snakeX -= 20;
+  } else if (direction == "up") {
+    snakeY -= 20;
+  } else if (direction == "down") {
+    snakeY += 20;
+  }
+
+  drawBackground();
+  drawSnake();
+}
+function changeDirection(event) {
+console.log("direction");
+}
+
+drawBackground();
+drawSnake();
+setInterval(update, 1000);
+addEventListener("keydown", changeDirection);
