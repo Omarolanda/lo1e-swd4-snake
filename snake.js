c1fc0b835d2b844c93b22a3b2e526f647917f655
Omarolanda <94382866+Omarolanda@users.noreply.github.com>
2022-09-13 13:09:19 +0200

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,10 +1,8 @@
-let canvas = document.getElementById('snake');
-let context = canvas.getContext('2d');
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
 
-// Draw the background
-context.fillStyle = "black";
-context.fillRect(0, 0, 400, 400);
+context.fillStyle = "lightblue";
+context.fillRect(0, 0 ,400, 400);
 
-// Draw the snake
-context.fillStyle = 'white';
-context.fillRect(200, 200, 20, 20);
+context.fillStyle = "aqua";
+context.fillRect(200, 200 ,40, 40);
